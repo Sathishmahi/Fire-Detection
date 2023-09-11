b7a33b6a7e0a7dc83990be1564e2dc8a08969798
Sathish Kumar <sathishmahi433@gmail.com>
2023-09-11 12:02:59 +0530

    Update init_setup.sh

--- init_setup.sh
+++ init_setup.sh
@@ -3,5 +3,5 @@ conda create -p venv python=3.10 -y
 echo "ACTIVATE ENV"
 source activate ./venv
 echo "DOWNLOAD REQUIREMENTS"
-pip install -r requirements_dev.txt
+pip install -r requirements.txt
 echo "FINSH DOWNLOAD REQUIREMENTS"
